5fa4600822591e6c5a25a86a96203905ae88d58f
Morris Buel <morris.buel@gmail.com>
2021-01-07 13:21:10 -0800

    final commit

---
+++ src/components/ListAppointments.js
@@ -0,0 +1,53 @@
+/* eslint-disable react/jsx-key */
+import React, {Component} from 'react';
+import { MdDeleteForever } from 'react-icons/md'
+import Moment from 'react-moment';
+
+class ListAppointments extends Component {
+
+    render() {
+        
+        return (
+            <div className="appointment-list item-list mb-3">
+            {this.props.appointments.map( item => (
+                <div className="pet-item col media py-3" key={item.aptId}>
+                <div className="mr-3">
+                    <button className="pet-delete btn btn-sm btn-danger"
+                        onClick={() => this.props.deleteAppointment(item)}
+                    ><MdDeleteForever /></button>
+                </div>
+    
+                <div className="pet-info media-body">
+                    <div className="pet-head d-flex">
+                        <span 
+                            className="pet-name"
+                            contentEditable
+                            suppressContentEditableWarning
+                            onBlur={
+                                e => this.props.updateInfo('petName', e.target.innerText, item.aptId)
+                            }>{item.petName}</span>
+                        <span className="apt-date ml-auto">
+                            <Moment
+                                date={item.aptDate}
+                                parse="YYYY-MM-DD hh:mm"
+                                format="Do MMM YYYY h:mma"
+                            />    
+                        </span>
+                    </div>
+    
+                    <div className="owner-name">
+                        <span className="label-item">Owner: </span>
+                        <span>{item.ownerName}</span>
+                    </div>
+                    <div className="apt-notes">{item.aptNotes}</div>
+                </div>
+                </div>
+            ))}
+
+        </div>
+  
+        );
+    }
+}
+
+export default ListAppointments;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.css";
+
+import './css/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +15,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
